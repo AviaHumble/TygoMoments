8f201012118ab1675e6175855f36cfa09d8925eb
William <aviahumble@gmail.com>
2021-02-19 18:07:51 +0000

    initial commit

---
+++ TygoMoments/Commands/Recursive.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TygoMoments.Commands
+{
+    class Recursive
+    {
+        public static void StartRecursive(int times)
+        {
+            for(int i = 0; i < times; i++)
+            {
+                List<string> momentsArray = new List<string> { "Cock", "Ok", "Lol", "what", "we big funny", "FAT", "Roboootootoototototo", "Yes", "No", "pp", "yesyes pp", "FAT", "robot.bald = miau miuo pio pio", "fun fact earth is a plate in a sphere", "i do be faLLIN DOE", "in spaxce here", "/ban @robotdance23", "wafels", "carlos", "stop", "Hello! Welcome to Central Hospital! How can I help you?", "ur so old big grandpa", "BOBOT" };
+                Random random = new Random();
+                int index = random.Next(momentsArray.Count);
+                Console.WriteLine(momentsArray[index]);
+            }
+        }
+    }
+}
--- TygoMoments/Program.cs
+++ TygoMoments/Program.cs
@@ -28,6 +28,14 @@ namespace TygoMoments
             if(arguments[1] == "--help" || arguments[1] == "-h")
             {
                 Commands.Help.HelpInit();
+            } else if(arguments[1] == "--generate" || arguments[1] == "-m")
+            {
+                Commands.Generate.GenerateRun();
+            } else if (arguments[1] == "--recursive" || arguments[1] == "-r")
+            {
+                Console.WriteLine("How many do you want? PS. Loads = u want to kill urself");
+                int times = int.Parse(Console.ReadLine());
+                Commands.Recursive.StartRecursive(times);
             }
         }
     }
